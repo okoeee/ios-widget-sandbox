1c9e3b0fc2cb0ad8af5b9fa6ea903ada0cb463ad
okoeee <yk27.puxcm@gmail.com>
2024-12-29 16:53:39 +0900

    Initial Commit

---
+++ WidgetSandbox/WidgetSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetSandboxApp.swift
+//  WidgetSandbox
+//
+//  Created by 横江一真 on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct WidgetSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WidgetSandboxTests/WidgetSandboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetSandboxTests.swift
+//  WidgetSandboxTests
+//
+//  Created by 横江一真 on 2024/12/29.
+//
+
+import Testing
+@testable import WidgetSandbox
+
+struct WidgetSandboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
